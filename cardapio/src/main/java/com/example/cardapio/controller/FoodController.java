04cd43254eea60bf436e3bc1febebc2b0de40891
Shystra <jlsp.2019@hotmail.com>
2024-01-16 20:58:25 -0300

    feat: new function using GetMapping for create new responses from users

--- cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -2,11 +2,14 @@ package com.example.cardapio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.example.cardapio.food.Food;
 import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
 import com.example.cardapio.food.FoodResponseDTO;
 
 import java.util.List;
@@ -19,6 +22,15 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
     
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        //Criar um novo cardápio de alimentos no banco de dados
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+
+    }
+
     @GetMapping
     public List<FoodResponseDTO> getAll(){
         List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
